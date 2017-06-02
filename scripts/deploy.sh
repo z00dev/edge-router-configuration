f8197d4a3e2d995727e758851fe7225b9d513e8d
Fred Cox <frederick.j.cox@gmail.com>
2017-06-02 16:07:08 +0300

    decrypt file in main

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,6 @@ set -e
 test $TRAVIS_BRANCH = "master"
 test $TRAVIS_PULL_REQUEST = "false"
 eval $(ssh-agent -s)
-openssl aes-256-cbc -K $encrypted_f5a3225ce721_key -iv $encrypted_f5a3225ce721_iv -in fire.pem.enc -out fire.pem -d
-chmod 0400 fire.pem
 ssh-add fire.pem
 ssh mcfedr@fire.ekreative.com ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -H edge.ekreative.com 2>&1 | tee -a $HOME/.ssh/known_hosts
 ssh -o ProxyCommand='ssh -q mcfedr@fire.ekreative.com nc -q0 %h %p' admin@edge.ekreative.com < build/dhcp.sh
