2b005d402d130a4e29493d1092e09d5ff8e22077
Fred Cox <frederick.j.cox@gmail.com>
2017-06-02 16:10:22 +0300

    keytypes not visible

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ test $TRAVIS_BRANCH = "master"
 test $TRAVIS_PULL_REQUEST = "false"
 eval $(ssh-agent -s)
 ssh-add fire.pem
-ssh mcfedr@fire.ekreative.com ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -H edge.ekreative.com 2>&1 | tee -a $HOME/.ssh/known_hosts
+ssh mcfedr@fire.ekreative.com ssh-keyscan -H edge.ekreative.com 2>&1 | tee -a $HOME/.ssh/known_hosts
 ssh -o ProxyCommand='ssh -q mcfedr@fire.ekreative.com nc -q0 %h %p' admin@edge.ekreative.com < build/dhcp.sh
 ssh -o ProxyCommand='ssh -q mcfedr@fire.ekreative.com nc -q0 %h %p' admin@edge.ekreative.com < build/dns.sh
