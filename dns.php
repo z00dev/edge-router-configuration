306a711942f49b6ba0145575bc4d5cdd4a2dc6bf
Fred Cox <frederick.j.cox@gmail.com>
2017-09-08 16:36:22 +0300

    need to specify source ip address

--- dns.php
+++ dns.php
@@ -12,7 +12,7 @@ source /opt/vyatta/etc/functions/script-template
 configure
 delete service dns forwarding options
 set service dns forwarding options "listen-address=10.0.1.1"
-set service dns forwarding options server=/internal.kidslox.com/172.31.4.94
+set service dns forwarding options server=/internal.kidslox.com/172.31.4.94@10.0.1.1
 <?php
 while ($data = fgetcsv($f)) {
     if ($data[$ipCol] && $data[$dnsCol]) {
